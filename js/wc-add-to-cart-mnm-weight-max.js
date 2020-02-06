93081cfe6ff6468c15c6398664693026c5079f22
Kathy Daring <helgatheviking@gmail.com>
2020-02-05 19:20:48 -0700

    initial commit

---
+++ js/wc-add-to-cart-mnm-weight-max.js
@@ -0,0 +1,124 @@
+( function( $ ) {
+
+	/**
+	 * Main container object.
+	 */
+	function WC_MNM_Weight( container ) {
+
+		var self       = this;
+		this.container = container;
+
+		/**
+		 * Init.
+		 */
+
+		this.initialize = function() {
+			
+			if( container.$mnm_cart.data( 'max_weight' ) !== 'undefined' ) {
+				this.bind_event_handlers();
+				this.add_error_div();			
+			}
+
+		};
+
+		/**
+		 * Add error div.
+		 */
+		this.add_error_div = function( container ) {
+			// Add a div to hold the error message.
+			var $error = this.container.$mnm_cart.find( '.wc-mnm-weight-counter' );
+
+			if ( ! $error.length ){
+				$('<div class="wc-mnm-weight-counter"></div>').insertBefore( this.container.$mnm_price );
+			}
+		};
+
+		/**
+		 * Container-Level Event Handlers.
+		 */
+		this.bind_event_handlers = function() {
+			$( '.mnm_form' ).on( 'wc-mnm-updated-totals', this.update_totals );
+			$( '.mnm_form' ).on( 'wc-mnm-validation',     this.validate );
+		};
+
+		/**
+		 * Update Totals.
+		 */
+		this.update_totals = function( event, container ) {
+			var total_weight  = 0;
+
+			$.each( container.child_items, function( index, child_item ) {
+
+				item_weight = parseFloat( child_item.$self.find( '.product-weight' ).data( 'weight' ) );
+
+				if( typeof( item_weight ) === 'undefined' ) {
+					item_weight = 0;
+				}
+				total_weight += child_item.get_quantity() * item_weight;
+			} );
+
+			container.$mnm_cart.data( 'total_weight', total_weight );
+			container.$mnm_cart.find( '.wc-mnm-weight-counter' ).html( self.get_weight_html( true ) );
+		};
+
+		/**
+		 * Validate Weight.
+		 */
+		this.validate = function( event, container ) {
+
+			container.reset_messages();
+
+			var total_weight = container.$mnm_cart.data( 'total_weight' );
+			if( typeof total_weight === 'undefined' ){
+				total_weight = 0;
+			}
+			var max_weight = container.$mnm_cart.data( 'max_weight' );
+
+
+			// If total weight is more than max weight.
+			if( total_weight > max_weight ) {
+				var message = wc_mnm_weight_params.i18n_max_weight_error.replace( '%max', self.get_formatted_weight( max_weight ) );
+				container.add_message( message, 'error' );
+			}
+		};
+
+		/**
+		 * Build the weight html component.
+		 */
+		this.get_formatted_weight = function( weight ) {
+			var unit  = self.container.$mnm_cart.data( 'weight_unit' );
+			return wc_mnm_weight_params.i18n_weight_format.replace( '%t', '' ).replace( '%w', wc_mnm_number_format( weight ) ).replace( '%u', unit );
+		};
+
+
+		/**
+		 * Build the weight html component.
+		 */
+		this.get_weight_html = function( show_total ) {
+
+			if( typeof( show_total ) === 'undefined' ) {
+				show_total = false;
+			}
+
+			var	weight_html  = '',
+				total_string = show_total ? '<span class="total">' + wc_mnm_weight_params.i18n_total + '</span>' : '',
+				formatted_weight = self.get_formatted_weight( container.$mnm_cart.data( 'total_weight' ) );
+
+			weight_html = wc_mnm_weight_params.i18n_weight_format.replace( '%t', total_string ).replace( '%w', formatted_weight ).replace( '%u', '' );
+			weight_html = '<p class="weight">' + weight_html + '</p>';
+
+			return weight_html;
+		};
+
+	} // End WC_MNM_Weight.
+
+	/*-----------------------------------------------------------------*/
+	/*  Initialization.                                                */
+	/*-----------------------------------------------------------------*/
+
+	$( '.mnm_form' ).on( 'wc-mnm-initializing', function( e, container ) {
+		var weight = new WC_MNM_Weight( container );
+		weight.initialize();
+	});
+
+} ) ( jQuery );
---
+++ wc-mnm-by-weight.php
@@ -0,0 +1,233 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Mix and Match: By Weight
+ * Plugin URI: http://www.woocommerce.com/products/woocommerce-mix-and-match-products/
+ * Description: Validate container by weight
+ * Version: 1.0.0
+ * Author: Kathy Darling
+ * Author URI: http://kathyisawesome.com/
+ * Developer: Kathy Darling, Manos Psychogyiopoulos
+ * Developer URI: http://kathyisawesome.com/
+ * Text Domain: wc-mnm-weight
+ * Domain Path: /languages
+ *
+ * Copyright: © 2018 Kathy Darling and Manos Psychogyiopoulos
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+
+
+/**
+ * The Main WC_MNM_Weight class
+ **/
+if ( ! class_exists( 'WC_MNM_Weight' ) ) :
+
+class WC_MNM_Weight {
+
+	/**
+	 * constants
+	 */
+	CONST VERSION = '1.0.0';
+	CONST REQUIRED_WOO = '3.3.0';
+
+	/**
+	 * WC_MNM_Weight Constructor
+	 *
+	 * @access 	public
+     * @return 	WC_MNM_Weight
+	 */
+	public static function init() {
+
+		// Load translation files.
+		add_action( 'init', array( __CLASS__, 'load_plugin_textdomain' ) );
+
+		// Add extra meta.
+		add_action( 'woocommerce_mnm_product_options', array( __CLASS__, 'container_weight_size_options') , 10, 2 );
+		add_action( 'woocommerce_admin_process_product_object', array( __CLASS__, 'process_meta' ), 20 );
+
+		// Display the weight on the front end.
+		add_action( 'woocommerce_mnm_child_item_details', array( __CLASS__, 'display_weight' ), 67, 2 );
+
+		// Register Scripts.
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'register_scripts' ) );
+		add_filter( 'woocommerce_mix_and_match_data_attributes', array( __CLASS__, 'add_data_attributes' ), 10, 2 );
+
+		// Display Scripts.
+		add_action( 'woocommerce_mix-and-match_add_to_cart', array( __CLASS__, 'load_scripts' ) );
+
+		// QuickView support.
+		add_action( 'wc_quick_view_enqueue_scripts', array( __CLASS__, 'load_scripts' ) );
+
+		// Add to cart validation.
+		add_filter( 'woocommerce_mnm_add_to_cart_validation', array( __CLASS__, 'weight_validation' ) );
+
+    }
+
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Localization */
+	/*-----------------------------------------------------------------------------------*/
+
+
+	/**
+	 * Make the plugin translation ready
+	 *
+	 * @return void
+	 */
+	public static function load_plugin_textdomain() {
+		load_plugin_textdomain( 'wc-mnm-weight' , false , dirname( plugin_basename( __FILE__ ) ) .  '/languages/' );
+	}
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Admin */
+	/*-----------------------------------------------------------------------------------*/
+
+
+	/**
+	 * Adds the container max weight option writepanel options.
+	 *
+	 * @param int $post_id
+	 * @param  WC_Product_Mix_and_Match  $mnm_product_object
+	 */
+	public static function container_weight_size_options( $post_id, $mnm_product_object ) {
+		woocommerce_wp_text_input( array(
+			'id'            => '_mnm_max_container_weight',
+			'label'       => __( 'Max Container Weight', 'wc-mnm-max-weight' ) . ' (' . get_option( 'woocommerce_weight_unit' ) . ')',
+			'desc_tip'    => true,
+			'description' => __( 'Maximum weight of containers in decimal form', 'woocommerce' ),
+			'type'        => 'text',
+			'data_type'   => 'decimal',
+			'value'			=> $mnm_product_object->get_meta( '_mnm_max_container_weight', true, 'edit' ),
+			'desc_tip'      => true
+		) );
+	}
+
+	/**
+	 * Saves the new meta field.
+	 *
+	 * @param  WC_Product_Mix_and_Match  $mnm_product_object
+	 */
+	public static function process_meta( $product ) {
+		if( ! empty( $_POST[ '_mnm_max_container_weight' ] ) ) {
+			$product->update_meta_data( '_mnm_max_container_weight', wc_clean( wp_unslash( $_POST[ '_mnm_max_container_weight' ] ) ) );
+		}
+	}
+
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Front End Display */
+	/*-----------------------------------------------------------------------------------*/
+
+	/**
+	 * Add the min/max attribute
+	 *
+	 * @param obj $mnm_product
+	 * @param obj $parent_product - the container product
+	 */
+	public static function display_weight( $mnm_product, $parent_product ){
+
+		if( $mnm_product->has_weight() ) {
+			printf( '<p class="product-weight" data-mnm-id="%d" data-weight="%s">%s</p>', $mnm_product->get_id(), $mnm_product->get_weight(), wc_format_weight( $mnm_product->get_weight() ) );
+		}
+		
+	}
+
+
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Cart Functions */
+	/*-----------------------------------------------------------------------------------*/
+
+
+	/**
+	 * Server-side weight validation
+	 * 
+	 * @param obj $mnm_item WC_Product of child item
+	 * @param obj WC_Mix_and_Match $product the parent container
+	 */
+	public static function weight_validation( $valid, $mnm_stock, $product ) {
+
+			$managed_items = $mnm_stock->get_managed_items();
+
+			$total_weight = 0;
+
+			foreach ( $managed_items as $managed_item_id => $managed_item ) {
+				$managed_product       = wc_get_product( $managed_item_id );
+				$item_title            = $managed_product->get_title();
+				$total_weight 		  += $managed_product->get_weight() * $managed_item[ 'quantity' ];
+			}
+
+			// Validate the total weight.
+			if ( $total_weight > $product->get_meta( '_mnm_max_container_weight' ) ) {
+				$error_message = sprintf( __( 'You &quot;%s&quot; is too heavy.', 'wc-mnm-max-weight' ), $product->get_title() );
+				wc_add_notice( $error_message, 'error' );
+				return false;
+			}
+
+			return true;
+	}
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Scripts and Styles */
+	/*-----------------------------------------------------------------------------------*/
+
+	/**
+	 * Register scripts
+	 *
+	 * @return void
+	 */
+	public static function register_scripts() {
+
+		wp_register_script( 'wc-add-to-cart-mnm-weight-max', plugins_url( 'js/wc-add-to-cart-mnm-weight-max.js', __FILE__ ), array( 'wc-add-to-cart-mnm' ), WC_MNM_Weight::VERSION, true );
+
+		$params = array(
+			'i18n_max_weight_error' => __( 'Your configuration is too heavy. Please choose less than %max to continue&hellip;', 'wc-mnm-weight' ),
+			'i18n_weight_format'    => sprintf( _x( '%1$s%2$s%3$s', '"Total Weight" string followed by weight followed by weight unit', 'woocommerce-mix-and-match-products' ), '%t', '%w', '%u' ),
+			'i18n_total'            => __( 'Total Weight: ', 'wc-mnm-weight' )
+		);
+
+		wp_localize_script( 'wc-add-to-cart-mnm-weight-max', 'wc_mnm_weight_params', $params );
+
+	}
+
+	/**
+	 * Script parameters
+	 *
+	 * @param  array $params
+	 * @param  obj WC_Mix_and_Match_Product
+	 * @return array
+	 */
+	public static function add_data_attributes( $params, $product ) {
+
+		if( $product->get_meta( '_mnm_max_container_weight' ) ) {
+
+			$new_params = array(
+				'max_weight'			=> $product->get_meta( '_mnm_max_container_weight', true, 'edit' ),
+				'weight_unit' 			=> get_option( 'woocommerce_weight_unit' )
+			);
+
+			$params = array_merge( $params, $new_params );
+
+		}
+
+		return $params;
+
+	}
+
+
+	/**
+	 * Load the script anywhere the MNN add to cart button is displayed
+	 * @return void
+	 */
+	public static function load_scripts(){
+		wp_enqueue_script( 'wc-add-to-cart-mnm-weight-max' );
+	}
+
+
+} //end class: do not remove or there will be no more guacamole for you
+
+endif; // end class_exists check
+
+// Launch the whole plugin.
+add_action( 'woocommerce_mnm_loaded', array( 'WC_MNM_Weight', 'init' ) );
